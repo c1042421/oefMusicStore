27040601d28a2d915c8cf76868aee2adc32c60fd
c1042421 <c1042421@G-D103-20>
2018-05-23 15:47:44 +0200

    exercise not done

--- MusicStore/Areas/Admin/Controllers/MaintainController.cs
+++ MusicStore/Areas/Admin/Controllers/MaintainController.cs
@@ -3,28 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using MusicStore.Data;
-using MusicStore.Models;
 using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MusicStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
-  
+    [Authorize(Roles = "Administrator")]
     public class MaintainController : Controller
     {
-        private readonly StoreContext _context;
-
-        public MaintainController(StoreContext context)
-        {
-            _context = context;
-        }
-
+        // GET: /<controller>/
         public IActionResult Index()
         {
-
             return View();
         }
     }
